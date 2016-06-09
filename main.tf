b05c030f6327fade22e5b2d5523e181d8c8358aa
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-09 11:51:51 -0700

    refresh

--- main.tf
+++ main.tf
@@ -6,15 +6,14 @@ module "imma" {
   source = "../env-imma"
 
   provider_region = "${var.provider_region}"
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  az_count = "${var.az_count}"
 
   vpc_name = "${var.vpc_name}"
   vpc_cidr = "${var.vpc_cidr}"
 
-  az_names = "${var.az_names}"
-
   nat_cidrs = "${var.nat_cidrs}"
-
-  bucket_remote_state = "${var.bucket_remote_state}"
-  context_org = "${var.context_org}"
-  context_env = "${var.context_env}"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,9 @@
+provider_region = "us-west-1"
 bucket_remote_state = "imma-remote-state"
 context_org = "imma"
 context_env = "dev"
 
-provider_region = "us-west-1"
-
-az_names = "us-west-1b us-west-1c"
+az_count = 2
 
 vpc_cidr = "172.31.0.0/16"
 
