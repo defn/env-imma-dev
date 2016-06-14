f9f6cab72d6a92bd5bc3d9342c9cc8561ad1b5a5
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-14 14:24:53 -0700

    removed provider_region var and use env aws defaults

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  region = "${var.provider_region}"
 }
 
 module "imma" {
   source = "../env-imma"
 
-  provider_region = "${var.provider_region}"
   bucket_remote_state = "${var.bucket_remote_state}"
   context_org = "${var.context_org}"
   context_env = "${var.context_env}"
