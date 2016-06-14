35b5a0747c76f652aeef97c239ae560b4ebe269e
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-13 18:17:32 -0700

    rely on wtf wrapper to define context

--- terraform.tfvars
+++
@@ -1,8 +0,0 @@
-provider_region = "us-west-1"
-bucket_remote_state = "imma-remote-state"
-context_org = "imma"
-context_env = "dev"
-
-az_count = 2
-
-vpc_cidr = "172.31.0.0/16"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "provider_region" {}
-variable "bucket_remote_state" {}
-variable "context_org" {}
-variable "context_env" {}
+variable "provider_region"     { default = "us-west-1" }
+variable "bucket_remote_state" { }
+variable "context_org"         { }
+variable "context_env"         { }
 
-variable "az_count" {}
+variable "az_count"            { default = 2 }
 
-variable "vpc_cidr" {}
-variable "vpc_name" {}
+variable "vpc_cidr"            { default = "172.31.0.0/16" }
+variable "vpc_name"            { }
